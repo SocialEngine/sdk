c801e0e9252ce757cf2fb6cbe61ea063358c4e97
RaymondBenc <me@raybenc.com>
2019-08-05 20:26:05 +0200

    Updating user entity.

--- .ide-helper/components/User/Entity.js
+++ .ide-helper/components/User/Entity.js
@@ -1,7 +1,32 @@
-/**
- * @property {number} id
- * @property {string} email
- */
-export default class UserEntity {
 
+export default class UserEntity {
+    /**
+     * @type {number}
+     */
+    get id () {}
+
+    /**
+     * @type {string}
+     */
+    get name () {}
+
+    /**
+     * @type {string}
+     */
+    get username () {}
+
+    /**
+     * @type {permalink}
+     */
+    get permalink () {}
+
+    /**
+     * @type {array}
+     */
+    get groups () {}
+
+    /**
+     * @type {boolean}
+     */
+    get isConnected () {}
 }
