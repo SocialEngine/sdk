ffc8365282823fbd445e988979d9b381cc8d1283
RaymondBenc <me@raybenc.com>
2019-08-13 01:28:50 +0200

    Adding /srv folder and bump version 1.0.1.

--- .ide-helper/webpack.config.js
+++ .ide-helper/webpack.config.js
@@ -3,7 +3,9 @@ const fs = require('fs');
 const path = require('path');
 
 const componentDir = path.join(__dirname, '/components');
-const alias = {};
+const alias = {
+    'app': path.join(__dirname, '/server/app.js')
+};
 
 for (const dir of fs.readdirSync(componentDir)) {
     alias['@SE/' + dir] = path.join(componentDir, dir);
